01d86d36030c370222216321f5ac9b8aa0c21e91
SoTypicalNoob <so.typical.noob@gmail.com>
2021-04-09 18:12:16 +0200

    New function: list_expired_items

--- shelf_life_checker.py
+++ shelf_life_checker.py
@@ -28,9 +28,32 @@ def list_database(filename):
         print(row)
 
 
+def list_expired_items(filename):
+    """Lists of the expired items.
+
+    Arg:
+        file: sqlite database
+
+    Returns:
+        Prints out the the expired items
+    """
+    connect = sqlite3.connect(filename)
+    cursor = connect.cursor()
+    cursor.execute(
+        """SELECT Brand.name, Product.name, Stock.expiring
+        FROM Stock JOIN Product JOIN Brand ON
+        Stock.product_id = Product.id AND
+        Product.brand_id = Brand.id
+        WHERE date(Stock.expiring) < date('now')"""
+    )
+    #WHERE date(Stock.expiring) < date('now')""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
 def main(filename):
-    """Starts list_database() in case this was started as a script."""
-    list_database(filename)
+    """Starts list_expired_item() in case this was started as a script."""
+    list_expired_items(filename)
 
 
 if __name__ == "__main__":
