8626a228df31c6938bfdfdd3e1bba7aeb8c96d9b
SoTypicalNoob <so.typical.noob@gmail.com>
2021-04-10 22:50:43 +0200

    New: lists items will be expired within a week

--- shelf_life_checker.py
+++ shelf_life_checker.py
@@ -26,6 +26,7 @@ def list_database(filename):
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+    return rows
 
 
 def list_expired_items(filename):
@@ -50,10 +51,35 @@ def list_expired_items(filename):
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+    return rows
+
+def list_expires_soon(filename):
+    """Lists of those items that expires within a week.
+
+    Arg:
+        file: sqlite database
+
+    Returns:
+        Prints out the items will be expire within a week.
+    """
+    connect = sqlite3.connect(filename)
+    cursor = connect.cursor()
+    cursor.execute(
+        """SELECT Brand.name, Product.name, Stock.expiring
+        FROM Stock JOIN Product JOIN Brand ON
+        Stock.product_id = Product.id AND
+        Product.brand_id = Brand.id
+        WHERE date(Stock.expiring) BETWEEN date('now') AND date('now', '+1 month')"""
+    )
+    #WHERE date(Stock.expiring) < date('now')""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    return rows
 
 def main(filename):
     """Starts list_expired_item() in case this was started as a script."""
-    list_expired_items(filename)
+    list_expires_soon(filename)
 
 
 if __name__ == "__main__":
