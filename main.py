ffbbbd2b1e55d1b0495ad48f52dd0aabfea491bc
SoTypicalNoob <so.typical.noob@gmail.com>
2021-04-29 16:51:38 +0200

    Deprecated features (add/remove) was deleted.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,40 +10,6 @@ def home():
     from shelf_life_checker import list_expires_soon
     soon_expire = list_expires_soon('shelf_life.sqlite3')
     return render_template("dashboard.html", expired=expired, soon_expire=soon_expire)
-
-
-@app.route("/add/", methods=["GET", "POST"])
-def add_item():
-    from shelf_life_checker import add_new_item
-    if request.method == "POST":
-        brand_name = request.form.get("brandName")
-        product_name = request.form.get("productName")
-        product_size = request.form.get("productSize")
-        product_unit = request.form.get("productUnit")
-        stock_expiring = request.form.get("stockExpiring")
-        stock_amount = request.form.get("stockAmount")
-    #from shelf_life_checker import add_new_item('shelf_life.sqlite3')
-        add_new_item('shelf_life.sqlite3', brand_name,
-                                    product_name, product_size, product_unit,
-                                    stock_expiring, stock_amount)
-    return render_template("add_item.html")
-
-
-@app.route("/delete/", methods=["GET", "POST"])
-def delete_item():
-    from shelf_life_checker import delete_item
-    if request.method == "POST":
-        brand_name = request.form.get("brandName")
-        product_name = request.form.get("productName")
-        product_size = request.form.get("productSize")
-        product_unit = request.form.get("productUnit")
-        stock_expiring = request.form.get("stockExpiring")
-        stock_amount = request.form.get("stockAmount")
-    #from shelf_life_checker import add_new_item('shelf_life.sqlite3')
-        delete_item('shelf_life.sqlite3', brand_name,
-                                    product_name, product_size, product_unit,
-                                    stock_expiring, stock_amount)
-    return render_template("delete_item.html")
 
 
 if __name__ == "__main__":
